0ffafe1517a39306a8dd1437e6f7bef90aa81a66
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2026-05-06 13:59:33 +0200

    Revert "Potential fix for pull request finding"
    This reverts commit 3eea1c87f6c9e0bfb1b78672711f64ac2d36957a.

--- app/saker/historikk/LeggTilHistorikkModal.tsx
+++ app/saker/historikk/LeggTilHistorikkModal.tsx
@@ -54,7 +54,7 @@ export function LeggTilHistorikkModal({ sakId, åpen, onClose }: LeggTilHistorik
     setDato(formaterDato(nå));
     setTid(formaterTid(nå));
     setSelected(nå);
-  }, [åpen, setSelected]);
+  }, [åpen]);
 
   function handleLagre(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
